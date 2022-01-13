faf43b5c8b4ad9dd9023209267b73affcdc3dfe1
Chloe Boylan <animaldna@gmail.com>
2022-01-13 10:27:58 -0500

    added rate limiter

--- app/main.py
+++ app/main.py
@@ -1,17 +1,30 @@
 from fastapi import FastAPI
 from app.routers import albums, artists, genres
 
+from starlette.applications import Starlette
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.middleware import SlowAPIMiddleware
+from slowapi.errors import RateLimitExceeded
+
+
+limiter = Limiter(key_func=get_remote_address, default_limits=["5/minute"])
 app = FastAPI()
+
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+app.add_middleware(SlowAPIMiddleware)
 
 app.include_router(albums.router)
 app.include_router(artists.router)
 app.include_router(genres.router)
 
 @app.get("/")
+@limiter.exempt
 def root():
     return {
         "hello": "Welcome to the Conehead Records API",
         "api_version": "v1",
-        "documentation_url": "thisapi.com/redoc"
+        "documentation_url": "https://catalog-api.chloeboylan.work/redoc"
     }
     
