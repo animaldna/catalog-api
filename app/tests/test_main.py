3056a47622652eac298729397e1bc41c5102d2b3
Chloe Boylan <animaldna@gmail.com>
2022-01-13 11:53:27 -0500

    updated tests and config.yml

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -9,7 +9,7 @@ def test_root_route():
     assert response.json() == {
         "hello": "Welcome to the Conehead Records API",
         "api_version": "v1",
-        "documentation_url": "https://catalog-api.chloeboylan.work"
+        "documentation_url": "https://catalog-api.chloeboylan.work/redoc"
     }
 
 def test_get_single_artist_success(sample_artist):
