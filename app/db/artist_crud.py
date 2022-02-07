f428a0d951c68866f49235ab1680b24f0125c16b
Chloe Boylan <animaldna@gmail.com>
2022-02-06 21:25:47 -0500

    Revert "switched to aioboto3 and updated to use async across the board"
    This reverts commit 0cb4f0358ab5a566b77dc07e00749448dad6bdbd.

--- app/db/artist_crud.py
+++ app/db/artist_crud.py
@@ -1,6 +1,4 @@
-import asyncio
-import aioboto3
-
+import boto3
 from . import models
 from typing import Optional
 from app.db.utilities import strip_ids
@@ -9,70 +7,60 @@ from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 
-async def get_single_artist(artist_id: str):
+ddb = boto3.resource('dynamodb')
+catalog = ddb.Table('catalog_v2')
+
+def get_single_artist(artist_id: str):
     """Query the main table with artist ID (as pk and sk)."""
     artist_key = f'artist#{artist_id}'
-    session = aioboto3.Session()
+    response = catalog.get_item(Key={'pk': artist_key, 'sk': artist_key})
+
+    try:
+        artist = models.Artist(**response['Item'], 
+            id=response['Item']['pk'].partition('#')[2])
+    except KeyError:
+        artist = None
     
-    async with session.resource('dynamodb') as ddb:
-        catalog = await ddb.Table('catalog_v2')
-        response = await catalog.get_item(
-            Key={'pk': artist_key, 'sk': artist_key}
-        )
-        try:
-            artist = models.Artist(**response['Item'], 
-                id=response['Item']['pk'].partition('#')[2])
-        except KeyError:
-            artist = None
-        
-        return artist      
+    return artist      
 
-async def get_all_artists():
+def get_all_artists():
     """Query the type_name_gsi GSI for all item_type=Artist.""" 
-    session = aioboto3.Session()
-    async with session.resource('dynamodb') as ddb:
-        catalog = await ddb.Table('catalog_v2')
-        response = await catalog.query(
-            IndexName='type_name_gsi',
-            KeyConditionExpression=Key('item_type').eq('Artist')
-        )
+    response = catalog.query(
+        IndexName='type_name_gsi',
+        KeyConditionExpression=Key('item_type').eq('Artist')
+    )
 
-        if response['Count'] > 0:
-            result = [strip_ids(item, 'artist') for item in response['Items']]
-        else:
-            result = None
-        
-        return result  
+    if response['Count'] > 0:
+        result = [strip_ids(item, 'artist') for item in response['Items']]
+    else:
+        result = None
+    
+    return result  
 
-async def get_artists_by_genre(genre: str):
-    session = aioboto3.Session()
-    async with session.resource('dynamodb') as ddb:
-        catalog = await ddb.Table('catalog_v2')
-        response = await catalog.query(
-            IndexName='genre_name',
-            KeyConditionExpression=Key('genre_id').eq(f'genre#{genre}') & 
-            Key('item_name').begins_with('artist#')
-        )
-        if response['Count'] > 0:
-            result = [strip_ids(item, 'artist') for item in response['Items']]
-        else:
-            result = None    
-        
-        return result
+def get_artists_by_genre(genre: str):
+    response = catalog.query(
+        IndexName='genre_name',
+        KeyConditionExpression=Key('genre_id').eq(f'genre#{genre}') & 
+        Key('item_name').begins_with('artist#')
+    )
+    if response['Count'] > 0:
+        result = [strip_ids(item, 'artist') for item in response['Items']]
+    else:
+        result = None    
+    
+    return result
 
-async def get_artist_albums(artist_id: str):
-    session = aioboto3.Session()
+def get_artist_albums(artist_id: str):
     artist_key = f'artist#{artist_id}'
-    async with session.resource('dynamodb') as ddb:
-        catalog = await ddb.Table('catalog_v2')
-        response = await catalog.query(
-            KeyConditionExpression=(Key('pk').eq(artist_key) & 
-                Key('sk').begins_with('album#'))
-        )
+    response = catalog.query(
+        KeyConditionExpression=(Key('pk').eq(artist_key) & 
+            Key('sk').begins_with('album#'))
+    )
 
-        if response['Items']:
-            result = [strip_ids(item,'album') for item in response['Items']]   
-        else: 
-            result = None
-        
-        return result
+    if response['Items']:
+        print(response['Items'])
+        result = [strip_ids(item,'album') for item in response['Items']]   
+    else: 
+        result = None
+    
+    return result
--- app/routers/artists.py
+++ app/routers/artists.py
@@ -1,22 +1,19 @@
-import asyncio
-import urllib.parse
-
 from fastapi import APIRouter, HTTPException, Path, Query
 from typing import List, Optional
 from app.internal.logger import logger
 from app.db import models,artist_crud as db
-
+import urllib.parse
 
 
 LOG = logger(__name__)
 router = APIRouter()
 
 @router.get("/artists/", response_model=List[models.Artist])
-async def get_artists(genre: Optional[str] = Query(None, min_length=7, max_length=7, regex="^[a-zA-Z0-9]+$")):
+def get_artists(genre: Optional[str] = Query(None, min_length=7, max_length=7, regex="^[a-zA-Z0-9]+$")):
     if genre is None:
-        artists = await db.get_all_artists()
+        artists = db.get_all_artists()
     else:
-        artists = await db.get_artists_by_genre(genre)
+        artists = db.get_artists_by_genre(genre)
 
     if artists is None:
         raise HTTPException(status_code=404, detail="No artists found.")
@@ -24,16 +21,16 @@ async def get_artists(genre: Optional[str] = Query(None, min_length=7, max_lengt
         return artists
 
 @router.get("/artists/{artist_id}", response_model=models.Artist)
-async def get_artist(artist_id: str = Path(..., min_length=7, max_length=7, regex="^[a-zA-Z0-9]+$")):
-    result = await db.get_single_artist(artist_id)
+def get_artist(artist_id: str = Path(..., min_length=7, max_length=7, regex="^[a-zA-Z0-9]+$")):
+    result = db.get_single_artist(artist_id)
     if result is None:
         raise HTTPException(status_code=404, detail="Artist not found")
     else:
         return result
 
 @router.get("/artists/{artist_id}/albums", response_model=List[models.Album])
-async def get_artist_albums(artist_id: str = Path(..., min_length=7, max_length=7, regex="^[a-zA-Z0-9]+$")):
-    artist_albums =  await db.get_artist_albums(artist_id)
+def get_artist_albums(artist_id: str = Path(..., min_length=7, max_length=7, regex="^[a-zA-Z0-9]+$")):
+    artist_albums =  db.get_artist_albums(artist_id)
     if artist_albums is None:
         raise HTTPException(status_code=404, detail="No albums found.")
     else:
