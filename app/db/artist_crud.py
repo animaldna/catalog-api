4a4595794201912f5ca14e21d7dd48c2913e4e06
Chloe Boylan <animaldna@gmail.com>
2022-02-11 14:59:10 -0500

    [ci skip] Restructure exceptions and relative module imports
    - Replace relative module import statements with dot notation
    - Restructure try/catch blocks in crud functions
    - Redirect logs to stdout and add additional logging in crud functions

--- app/db/artist_crud.py
+++ app/db/artist_crud.py
@@ -1,11 +1,14 @@
 import boto3
 from . import models
+from ..internal.logger import logger
+from ..db.utilities import strip_ids
+from fastapi import HTTPException
 from typing import Optional
-from app.db.utilities import strip_ids
 from boto3.dynamodb.conditions import Key, Attr
 from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
+LOG = logger(__name__)
 
 ddb = boto3.resource('dynamodb')
 catalog = ddb.Table('catalog_v2')
@@ -15,52 +18,77 @@ def get_single_artist(artist_id: str):
     artist_key = f'artist#{artist_id}'
     response = catalog.get_item(Key={'pk': artist_key, 'sk': artist_key})
 
-    try:
-        artist = models.Artist(**response['Item'], 
-            id=response['Item']['pk'].partition('#')[2])
-    except KeyError:
-        artist = None
-    
-    return artist      
+    try: 
+        response = catalog.get_item(Key={'pk': artist_key, 'sk': artist_key})
+    except ClientError as e:
+        LOG.error(e, exc_info=True)
+        raise HTTPException(status_code=500, detail=str(e))
+    else:
+        try:
+            artist = models.Artist(**response['Item'], 
+                id=response['Item']['pk'].partition('#')[2])
+        except KeyError:
+            LOG.info('No results found')
+            artist = None     
+        
+        return artist
+
 
 def get_all_artists():
     """Query the type_name_gsi GSI for all item_type=Artist.""" 
-    response = catalog.query(
-        IndexName='type_name_gsi',
-        KeyConditionExpression=Key('item_type').eq('Artist')
-    )
-
-    if response['Count'] > 0:
-        result = [strip_ids(item, 'artist') for item in response['Items']]
+    try:
+        response = catalog.query(
+            IndexName='type_name_gsi',
+            KeyConditionExpression=Key('item_type').eq('Artist')
+        )
+    except ClientError as e:
+        LOG.error(e, exc_info=True)
+        raise HTTPException(status_code=500, detail=str(e))
     else:
-        result = None
+        if response['Count'] > 0:
+            result = [strip_ids(item, 'artist') for item in response['Items']]
+        else:
+            # TODO -- Why would this happen? Need a better error.
+            LOG.error('Something went wrong.')
+            result = None
+        
+        return result  
     
-    return result  
 
 def get_artists_by_genre(genre: str):
-    response = catalog.query(
-        IndexName='genre_name',
-        KeyConditionExpression=Key('genre_id').eq(f'genre#{genre}') & 
-        Key('item_name').begins_with('artist#')
-    )
-    if response['Count'] > 0:
-        result = [strip_ids(item, 'artist') for item in response['Items']]
+    try:
+        response = catalog.query(
+            IndexName='genre_name',
+            KeyConditionExpression=Key('genre_id').eq(f'genre#{genre}') & 
+            Key('item_name').begins_with('artist#')
+        )
+    except ClientError as e:
+        LOG.error(e, exc_info=True)
+        raise HTTPException(status_code=500, detail=str(e))
     else:
-        result = None    
-    
-    return result
+        if response['Count'] > 0:
+            result = [strip_ids(item, 'artist') for item in response['Items']]
+        else:
+            result = None    
+        
+        return result
+
 
 def get_artist_albums(artist_id: str):
     artist_key = f'artist#{artist_id}'
-    response = catalog.query(
-        KeyConditionExpression=(Key('pk').eq(artist_key) & 
-            Key('sk').begins_with('album#'))
-    )
-
-    if response['Items']:
-        print(response['Items'])
-        result = [strip_ids(item,'album') for item in response['Items']]   
-    else: 
-        result = None
+    try: 
+        response = catalog.query(
+            KeyConditionExpression=(Key('pk').eq(artist_key) & 
+                Key('sk').begins_with('album#'))
+        )
+    except ClientError as e:
+        LOG.error(e, exc_info=True)
+        raise HTTPException(status_code=500, detail=str(e))
+    else:
+        if response['Items']:
+            result = [strip_ids(item,'album') for item in response['Items']]   
+        else: 
+            result = None
+        
+        return result
     
-    return result
--- app/db/genre_crud.py
+++ app/db/genre_crud.py
@@ -1,26 +1,38 @@
 import boto3
 from . import models
-from app.db.utilities import strip_ids
+from ..internal.logger import logger
+from ..db.utilities import strip_ids
+from fastapi import HTTPException
 from boto3.dynamodb.conditions import Key
+from botocore.exceptions import ClientError
 from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
+LOG = logger(__name__)
 
 ddb = boto3.resource('dynamodb')
 catalog = ddb.Table('catalog_v2')
 
 def get_all_genres():
-    response = catalog.query(
-        IndexName='type_name_gsi', 
-        KeyConditionExpression=Key('item_type').eq('Genre'),
-        ProjectionExpression="pk, genre_name"
-    )
-    try:
-        result = response['Items']
-    except KeyError:
-        result = None
+    try: 
+        response = catalog.query(
+            IndexName='type_name_gsi', 
+            KeyConditionExpression=Key('item_type').eq('Genre'),
+            ProjectionExpression="pk, genre_name"
+        )
+    except ClientError as e:
+        LOG.error(e, exc_info=True)
+        raise HTTPException(status_code=500, detail=str(e))
     else:
-        result = [strip_ids(item, 'genre') for item in response['Items']]
+        try:
+            result = response['Items']
+        except KeyError:
+            # TODO - This shouldn't happen. Need more info.
+            LOG.info('No results found')
+            result = None
+        else:
+            result = [strip_ids(item, 'genre') for item in response['Items']]
+        
+        return result
     
-    return result
     
